46869651580c85030c1bb7eab910161e674d5512
SurbhiSarvaiya <surbhigohil1991@gmail.com>
2026-01-19 10:27:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,3 +32,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
